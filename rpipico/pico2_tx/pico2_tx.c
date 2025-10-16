7bfca9d79c8836fce113136e5afaa9279f7cc291
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-10-16 00:25:18 -0300

    perf: primer aproximacion a estrategia de tx a 2mhz

--- rpipico/pico2_tx/pico2_tx.c
+++ rpipico/pico2_tx/pico2_tx.c
@@ -5,20 +5,26 @@
 #include "hardware/pwm.h"
 
 // GPIO por el que se envían los datos
-#define TX_GPIO     15
+#define TX_GPIO     26
 // GPIO para trigger
 #define TRIG_GPIO   16
 // Wrap para PWM (1000 ciclos de clock para 125KHz a partir de 125MHz)
-#define WRAP    1000
+#define WRAP    75
 
 /**
  * @brief Cantidad de microsegundos de ancho de pulso
  */
 typedef enum {
-    DUTY_BIT_ZERO = (uint16_t) (WRAP / 4),
-    DUTY_NO_BIT = (uint16_t) (WRAP / 2),
-    DUTY_BIT_ONE = (uint16_t) (3 * WRAP / 4)
+    DUTY_BIT_ZERO = (uint16_t) (0.5 * WRAP),
+    DUTY_NO_BIT = (uint16_t) (0.5 * WRAP),
+    DUTY_BIT_ONE = (uint16_t) (0.5 * WRAP)
 } duty_us_t;
+
+typedef enum {
+    CYCLES_BIT_ZERO = 6,
+    CYCLES_NO_BIT = 9,
+    CYCLES_BIT_ONE = 12
+} cycles_t;
 
 /**
  * Estructura de control para la trama de datos
@@ -26,6 +32,7 @@ typedef enum {
 typedef struct {
     uint16_t duty;  // Marca para el ancho de pulso
     bool next_bit;  // Booleano para habilitar el siguiente bit
+    cycles_t cycles;// Cantidad de ciclos
 } duty_control_t;
 
 // Numero de slice de PWM
@@ -40,7 +47,6 @@ void on_wrap(void) {
     // Limpio flag
     pwm_clear_irq(slice);
     // Cambio el duty cicle y destrabo main
-    pwm_set_gpio_level(TX_GPIO, control.duty);
     control.next_bit = true;
 }
 
@@ -48,19 +54,19 @@ void on_wrap(void) {
  * @brief Programa principal
  */
 int main(void) {
-    // Clock del sistema en 125MHz
-    set_sys_clock_khz(125000, true);
+    // Clock del sistema en 100MHz
+    set_sys_clock_khz(150000, true);
     stdio_init_all();
 
     uint16_t test_data[4] = {0xa796, 0xabcd, 0x1234, 0x5678};
     uint8_t test_data_index = 0;
     uint16_t data;
 
-    // Inicializacion de PWM a 125KHz
+    // Inicializacion de PWM a 2MHz
     gpio_set_function(TX_GPIO, GPIO_FUNC_PWM);
     pwm_config config = pwm_get_default_config();
     slice = pwm_gpio_to_slice_num(TX_GPIO);
-    pwm_config_set_wrap(&config, 1000);
+    pwm_config_set_wrap(&config, WRAP);
     // Interrupcion de PWM en cada wrap
     pwm_clear_irq(slice);
     pwm_set_irq_enabled(slice, true);
@@ -88,10 +94,21 @@ int main(void) {
     	for(uint8_t i = 0; i < 16; i++) {
             // Asigno el ancho de pulso segun si es 1 o 0
     		control.duty = (data & (1 << (15 - i)))? DUTY_BIT_ONE : DUTY_BIT_ZERO;
-            // Apago el flag
-            control.next_bit = false;
-            // Espero a que este lista la interrupcion
-            while(!control.next_bit);
+            control.cycles = (data & (1 << (15 - i)))? CYCLES_BIT_ONE : CYCLES_BIT_ZERO;
+            uint8_t cycles = (uint8_t) control.cycles;
+            
+            for(uint16_t i = 0; i < 16; i++) {
+
+                if(i < control.cycles) {
+                    pwm_set_gpio_level(TX_GPIO, control.duty);
+                }
+                else {
+                    pwm_set_gpio_level(TX_GPIO, 0);
+                }
+                control.next_bit = false;
+                // Espero a que este lista la interrupcion
+                while(!control.next_bit);
+            }
     	}
     
     #ifdef TRIG_GPIO
@@ -100,12 +117,24 @@ int main(void) {
     
         // Fin de trama
         control.duty = DUTY_NO_BIT;
-        // Apago el flag
-        control.next_bit = false;
-        // Espero a que este lista la interrupcion
-        while(!control.next_bit);
+        control.cycles = CYCLES_NO_BIT;
+        uint8_t cycles = (uint8_t) control.cycles;
+
+        for(uint16_t i = 0; i < 16; i++) {
+
+            if(i < control.cycles) {
+                pwm_set_gpio_level(TX_GPIO, control.duty);
+            }
+            else {
+                pwm_set_gpio_level(TX_GPIO, 0);
+            }
+            control.next_bit = false;
+            // Espero a que este lista la interrupcion
+            while(!control.next_bit);
+        }
+        
+        // Iteración de datos
         test_data_index = (test_data_index + 1) % 4;
-        sleep_ms(5);
     }
     return 0;
 }
