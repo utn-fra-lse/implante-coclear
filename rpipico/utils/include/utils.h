3f9a9850dbae38d5d3564f2c6746a6cc6659cd3d
FrancoLopez0 <francoalelopez@gmail.com>
2026-07-10 12:28:43 -0300

    feat(utils): add fft_usb_packet_t struct and usb transmission function

--- rpipico/utils/include/utils.h
+++ rpipico/utils/include/utils.h
@@ -15,8 +15,17 @@ void init_pwm_test(uint gpio_pin, uint16_t freq_hz);
 
 void generate_sample_buffer(uint8_t *buffer, uint16_t size, uint32_t sample_rate, uint8_t offset);
 
-void send_freqs_magnitude(float *magnitudes, uint16_t size, uint16_t freq_bin_width);
+#ifndef FFT_SIZE
+#define FFT_SIZE 512U
+#endif
 
-void send_fft_data_binary(uint16_t *magnitudes, uint16_t fft_size, uint32_t sample_rate);
+typedef struct __attribute__((packed, aligned(4))) {
+    uint8_t  sync[2];       // 0xAA, 0x55
+    uint16_t num_bins;
+    uint32_t sample_rate;
+    float    real_part[FFT_SIZE / 2];
+    float    imag_part[FFT_SIZE / 2];
+} fft_usb_packet_t;
 
+void send_fft_data_usb(const fft_usb_packet_t *packet);
 #endif
